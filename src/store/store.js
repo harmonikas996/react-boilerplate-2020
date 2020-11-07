90c2b7b8e25e7baec5ae46d7a34a03a67725a9fd
harmonikas996 <lazarevacharmonika@gmail.com>
2020-11-07 18:50:01 +0100

    Implemented Redux Logger middleware. Active logging when not in PRODUCTION environment.

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../components/Counter/counterSlice';
+import { createLogger } from 'redux-logger';
+
+const middlewares = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger());
+}
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: middlewares,
 });
